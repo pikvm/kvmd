00620c86f34b1ff371c358b1951e860fd5405800
Tomasz Duda <tomaszduda23@gmail.com>
2022-07-05 21:56:15 +0200

    stm keyboard WIP

--- hid/lib/stm32/usb/stm32-keyboard.h
+++ hid/lib/stm32/usb/stm32-keyboard.h
@@ -2,14 +2,37 @@
 
 #include "keyboard.h"
 #include "proto.h"
+#include <USBComposite.h>
 
-//TODO implement
+// TODO implement
 
 namespace kvmd::stm32
 {
     class UsbKeyboard : public Keyboard
     {
     public:
+        UsbKeyboard() = delete;
+
+        UsbKeyboard(USBHID *_hid) : _hid(_hid), _kbd(*_hid, 0)
+        {
+        }
+
         uint8_t getType() { return PROTO::OUTPUTS1::KEYBOARD::USB; }
+
+        void clear()
+        {
+            _kbd.releaseAll();
+        }
+
+        void begin()
+        {
+            _hid->begin(HID_BOOT_KEYBOARD);
+            _kbd.begin();
+        }
+
+    private:
+        USBHID *_hid;
+        HIDKeyboard _kbd;
     };
+
 }
