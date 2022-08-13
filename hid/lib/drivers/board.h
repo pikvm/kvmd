c1874d64c170a7b5a7062b23c7a81dfd828546fc
Tomasz Duda <tomaszduda23@gmail.com>
2022-08-13 16:58:22 +0200

    add status led

--- hid/lib/drivers/board.h
+++ hid/lib/drivers/board.h
@@ -25,9 +25,18 @@
 #include "driver.h"
 
 namespace DRIVERS {
+	enum status {
+		RX_DATA = 0,
+		KEYBOARD_ONLINE,
+		MOUSE_ONLINE,
+	};
+
 	struct Board : public Driver {
+
 		using Driver::Driver;
+
 		virtual void reset() {}
 		virtual void periodic() {}
+		virtual void updateStatus(status status) {}
 	};
 }
