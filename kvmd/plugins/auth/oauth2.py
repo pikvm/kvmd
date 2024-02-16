e9f1779405044f4a719ed12b31a88f2c68ee0fe4
Markus Beckschulte <markus.beckschulte@alumni.fh-aachen.de>
2024-02-16 12:37:00 +0100

    add license

--- kvmd/plugins/auth/oauth2.py
+++ kvmd/plugins/auth/oauth2.py
@@ -1,3 +1,25 @@
+# ========================================================================== #
+#                                                                            #
+#    KVMD - The main PiKVM daemon.                                           #
+#                                                                            #
+#    Copyright (C) 2018-2024  Maxim Devaev <mdevaev@gmail.com>               #
+#                  2024-2024  Markus Beckschulte (SLA/RWTH Aachen)           #
+#    This program is free software: you can redistribute it and/or modify    #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 3 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    This program is distributed in the hope that it will be useful,         #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    You should have received a copy of the GNU General Public License       #
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                            #
+# ========================================================================== #
+
+
 from typing import Any
 from urllib.parse import urlencode
 import secrets
