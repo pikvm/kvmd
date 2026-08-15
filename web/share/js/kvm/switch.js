901bf01f021eaab9d2d8b669ed2151a1673aadd8
Maxim Devaev <mdevaev@gmail.com>
2026-08-15 09:24:25 +0300

    web: replace "server" to "host"

--- web/share/js/kvm/switch.js
+++ web/share/js/kvm/switch.js
@@ -618,7 +618,7 @@ export function Switch() {
 		if ($("switch-atx-ask-switch").checked) {
 			wm.confirm(`
 				Are you sure you want to press the <b>${tools.escape(button)}</b> button?<br>
-				Warning! This could cause data loss on the server.
+				Warning! This could cause data loss on the target host.
 			`).then(function(ok) {
 				if (ok) {
 					click_button();
